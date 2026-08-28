409bfb8333a1e766042652bb576e1ffdd58e6c0c
manoj <manojr3690@gmail.com>
2026-08-28 20:12:00 +0530

    Move Terraform files into terraform directory

--- internet_gateway.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_internet_gateway" "kartify_internet_gateway" {
-  vpc_id = aws_vpc.kartify_vpc.id
-
-  tags = {
-    Name        = "${var.project_name}-internet-gateway"
-    Environment = var.environment
-    ManagedBy   = "Terraform"
-  }
-}
