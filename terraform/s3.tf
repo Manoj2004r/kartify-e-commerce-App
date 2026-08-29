340449c114463ec6ae1d66fd7db0271ff1a26de5
Manoj R <159867646+Manoj2004r@users.noreply.github.com>
2026-08-29 19:28:37 +0530

    Add dependency to S3 bucket policy

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -30,7 +30,9 @@ resource "aws_s3_bucket_public_access_block" "frontend_bucket_public_access" {
 
 resource "aws_s3_bucket_policy" "frontend_bucket_policy" {
   bucket = aws_s3_bucket.frontend_bucket.id
-
+  depends_on = [
+    aws_s3_bucket_public_access_block.frontend_bucket_public_access
+  ]
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
