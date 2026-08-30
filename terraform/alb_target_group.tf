5eb35880bb85ee336fb0189f936544d8a7e0bbe6
Manoj R <159867646+Manoj2004r@users.noreply.github.com>
2026-08-30 19:27:18 +0530

    Change ALB target group port from 8080 to 5000

--- terraform/alb_target_group.tf
+++ terraform/alb_target_group.tf
@@ -1,6 +1,6 @@
 resource "aws_lb_target_group" "backend" {
   name        = "${var.project_name}-backend-tg"
-  port        = 8080
+  port        = 5000
   protocol    = "HTTP"
   target_type = "ip"
   vpc_id      = aws_vpc.kartify_vpc.id
